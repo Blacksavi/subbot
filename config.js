cfc0a462b864285e6e1cadb9d99bbc32b26e0e4a
SAVI <87853311+Blacksavi@users.noreply.github.com>
2025-02-26 16:58:34 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  token: "YOUR_TELEGRAM_BOT_TOKEN", // Replace with your bot's token
+  apiHash: "YOUR_API_HASH", // Optional: If using Telegram API
+  apiId: "YOUR_API_ID", // Optional: If using Telegram API
+};
