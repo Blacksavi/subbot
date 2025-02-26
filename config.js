ab8fa4a54c95ac4b7b20ed8290df905e4b1e03d7
SAVI <87853311+Blacksavi@users.noreply.github.com>
2025-02-26 17:18:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "YOUR_TELEGRAM_BOT_TOKEN", // Replace with your bot's token
-  apiHash: "YOUR_API_HASH", // Optional: If using Telegram API
-  apiId: "YOUR_API_ID", // Optional: If using Telegram API
+  token: "7024753561:AAH8apgeVnYkt9BGZ-4DrwCPTueqkyGOy_g", // Replace with your bot's token
+  apiHash: "dfba8e798dbc7e0174d4f633462ffa64", // Optional: If using Telegram API
+  apiId: "21001356", // Optional: If using Telegram API
 };
